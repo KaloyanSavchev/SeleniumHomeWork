fe120d2c1c90f4b1584d72e297a297576869aced
Kaloyan <116354585+KaloyanSavchev@users.noreply.github.com>
2025-02-12 23:12:43 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumTest
+{
+
+    public class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            var driverOptions = new ChromeOptions();
+            driverOptions.AddArgument("--start-maximized");
+            driver = new ChromeDriver();
+            driver.Url = "https://www.ozone.bg/";
+
+            
+        }
+
+        [Test]
+        public void TitleLoading_When_SiteLoad()
+        {
+            Thread.Sleep(2000);
+            string expectedTitle = "Mагазин за игри, книги, геймърски аксесоари и играчки | Ozone.bg";
+            string actualTitle = driver.Title; 
+            Console.WriteLine(expectedTitle, actualTitle);
+          
+            Assert.AreEqual(expectedTitle, actualTitle, "The title is not as expected!");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+           driver.Quit();
+        }
+    }
+}
